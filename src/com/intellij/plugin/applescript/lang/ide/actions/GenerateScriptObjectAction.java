b469866266100b478519bb12bfa311cc758c0243
Andrey <andrey.dernov@gmail.com>
2016-01-17 01:12:18 +0300

    Generate script action (with no logic yet :-)

---
+++ src/com/intellij/plugin/applescript/lang/ide/actions/GenerateScriptObjectAction.java
@@ -0,0 +1,15 @@
+package com.intellij.plugin.applescript.lang.ide.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by Andrey on 17/01/16.
+ */
+public class GenerateScriptObjectAction extends AnAction {
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    // TODO: insert action logic here
+  }
+}
