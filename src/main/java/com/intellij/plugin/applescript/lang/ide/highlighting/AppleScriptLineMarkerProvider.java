b2e451716a2ec9f60e0d45b502e335d0de756c8b
Andrey Dernov <andrey.dernov@jetbrains.com>
2017-11-13 16:35:19 +0300

    LineMarker is supposed to be registered for leaf elements only

--- src/main/java/com/intellij/plugin/applescript/lang/ide/highlighting/AppleScriptLineMarkerProvider.java
+++ src/main/java/com/intellij/plugin/applescript/lang/ide/highlighting/AppleScriptLineMarkerProvider.java
@@ -9,6 +9,7 @@ import com.intellij.plugin.applescript.lang.ide.sdef.AppleScriptProjectDictionar
 import com.intellij.plugin.applescript.lang.sdef.ApplicationDictionary;
 import com.intellij.plugin.applescript.psi.AppleScriptApplicationReference;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -17,10 +18,12 @@ public class AppleScriptLineMarkerProvider extends RelatedItemLineMarkerProvider
 
   @Override
   protected void collectNavigationMarkers(@NotNull PsiElement element,
-                                          Collection<? super RelatedItemLineMarkerInfo> result) {
+                                          @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
     if (element instanceof AppleScriptApplicationReference) {
+      PsiElement leafNode = PsiTreeUtil.firstChild(element);
+      if (leafNode == null) return;
       AppleScriptProjectDictionaryService dictionaryService = ServiceManager.getService(element.getProject(),
-              AppleScriptProjectDictionaryService.class);
+          AppleScriptProjectDictionaryService.class);
       AppleScriptApplicationReference appRef = (AppleScriptApplicationReference) element;
       String appName = appRef.getApplicationName();
       if (dictionaryService == null || StringUtil.isEmpty(appName)) return;
@@ -28,13 +31,10 @@ public class AppleScriptLineMarkerProvider extends RelatedItemLineMarkerProvider
       if (dictionary == null /*|| dictionary.getApplicationBundle() == null*/) return;
 
       NavigationGutterIconBuilder<PsiElement> builder =
-              NavigationGutterIconBuilder.create(dictionary.getIcon(0)).
-                      setTargets(dictionary).
-                      setTooltipText("Navigate to application dictionary file");
-      result.add(builder.createLineMarkerInfo(element));
+          NavigationGutterIconBuilder.create(dictionary.getIcon(0)).
+              setTargets(dictionary).setTooltipText("Navigate to application dictionary file");
+      result.add(builder.createLineMarkerInfo(leafNode));
 
     }
-
-    super.collectNavigationMarkers(element, result);
   }
 }
